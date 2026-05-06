93e6987eda8ab83e9974d489962b02661ce58447
oswaldo32s <oswaldo32gonzalez@gmail.com>
2026-05-05 21:49:13 -0600

    Create Piece class and costructor

--- Models/Piece.cs
+++ Models/Piece.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Chess.Models
 {
-    internal class Piece
-    {
+    public enum  PieceType {
+        King, Queen, Bishop, Tower, Knight, Pawn
+    }
+
+    public enum Color {
+        White, Black
+    }
+
+    public class Piece {
+        public Color Color { get; set; }
+        public PieceType Type { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Piece(PieceType type, Color color)
+        {
+            Type = type;
+            Color = color;
+        }
     }
 }
